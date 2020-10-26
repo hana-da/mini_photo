3e83f5a8a169ec0494cd701c1df1b3c18d174470
hana-da <hana-da@users.noreply.github.com>
2020-10-26 19:35:01 +0900

    Setup Rails i18n

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Rails.application.config.i18n.load_path        += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+Rails.application.config.i18n.default_locale    = :ja
+Rails.application.config.i18n.available_locales = %i[ja en]
