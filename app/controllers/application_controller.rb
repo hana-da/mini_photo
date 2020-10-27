d02fb13a46f13d1645e5adcff8098f135806f283
hana-da <hana-da@users.noreply.github.com>
2020-10-27 20:30:37 +0900

    Add the authorization mechanism

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  private def login_required!
+    redirect_to new_user_session_path unless logged_in?
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module SessionsHelper
+  # @return [User,nil] 現在ログインしているUser、ログインしていない場合はnil
+  private def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  # @return [true, false] 現在ログインしているか
+  private def logged_in?
+    current_user.instance_of?(User)
+  end
 end
