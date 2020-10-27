54ca25d52744082d43c0eec6b7b74c9256f41de9
hana-da <hana-da@users.noreply.github.com>
2020-10-27 19:36:54 +0900

    Add SessionsController#create for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,24 @@ class SessionsController < ApplicationController
   def new
     @form = LoginForm.new
   end
+
+  def create
+    @form = LoginForm.new(params.required(:login_form).permit(:username, :password))
+    return render :new unless @form.valid?
+
+    user = User.find_by(name: @form.username)
+    if user&.authenticate(@form.password)
+      login(user)
+      redirect_to new_user_session_path
+    else
+      @form.errors.add(:base, 'ユーザーIDまたはパスワードが違います。')
+      render :new
+    end
+  end
+
+  private def login(user)
+    reset_session
+
+    session[:user_id] = user.id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   resource :user, only: [] do
-    resource :session, only: :new
+    resource :session, only: %i[new create]
   end
 end
