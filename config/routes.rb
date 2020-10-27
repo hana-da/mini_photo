36891db9d399c3ccd8b272cf5b80e17dc3dcdb1f
hana-da <hana-da@users.noreply.github.com>
2020-10-27 18:56:31 +0900

    Add SessionsController#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :user, only: [] do
+    resource :session, only: :new
+  end
 end
