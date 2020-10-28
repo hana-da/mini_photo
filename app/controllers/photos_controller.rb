ad175491a6abd34a1a5e0addccb356e0e9bec8f0
hana-da <hana-da@users.noreply.github.com>
2020-10-28 09:11:10 +0900

    Add PhotosController#create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,15 @@ class PhotosController < ApplicationController
   def new
     @photo = current_user.photos.build
   end
+
+  def create
+    photo_params = params.required(:photo).permit(:title, :image_file)
+    @photo = current_user.photos.build(photo_params)
+
+    if @photo.save
+      redirect_to user_photos_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   resource :user, only: [] do
     resource :session, only: %i[new create]
-    resources :photos, only: %i[index new]
+    resources :photos, only: %i[index new create]
   end
 end
